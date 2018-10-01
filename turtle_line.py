b8982e1f7ba459d022b635bfc0f8dd37bf10c4f3
David Aroesti <josedavid.aroesti@gmail.com>
2018-09-30 19:43:51 -0500

    turtle examples

---
+++ turtle_line.py
@@ -0,0 +1,7 @@
+import turtle
+
+tortuga = turtle.Turtle()
+
+tortuga.forward(50)
+
+turtle.done()
---
+++ turtle_square.py
@@ -0,0 +1,17 @@
+import turtle
+
+tortuga = turtle.Turtle()
+
+tortuga.forward(50)
+tortuga.right(90)     # Rotate clockwise by 90 degrees
+
+tortuga.forward(50)
+tortuga.right(90)
+
+tortuga.forward(50)
+tortuga.right(90)
+
+tortuga.forward(50)
+tortuga.right(90)
+
+turtle.done()
