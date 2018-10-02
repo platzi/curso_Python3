37b117e4d18223e17d8253f5ae9a2e5ea44b9f58
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-02 08:15:16 -0500

    Clients persistence

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import sys
+import csv
+import os
 
 
-clients = [
-    {
-        'name': 'Pablo',
-        'company': 'Google',
-        'email': 'pablo@google.com',
-        'position': 'Software Engineer',
-    },
-    {
-        'name': 'Ricardo',
-        'company': 'Facebook',
-        'email': 'ricardo@facebook.com',
-        'position': 'Data Engineer',
-    },
-]
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+CLIENT_TABLE = '.clients.csv'
+clients = []
 
 
 def create_client(client):
@@ -85,6 +75,24 @@ def _get_client_from_user():
     return client
 
 
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+        for row in reader:
+            clients.append(row)
+
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+        writer.writerows(clients)
+
+        os.remove(CLIENT_TABLE)
+        os.rename(tmp_table_name, CLIENT_TABLE)
+
+
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50)
@@ -97,6 +105,7 @@ def _print_welcome():
 
 
 if __name__ == '__main__':
+    _initialize_clients_from_storage()
     _print_welcome()
 
     command = input()
@@ -106,7 +115,6 @@ if __name__ == '__main__':
         client = _get_client_from_user()
 
         create_client(client)
-        list_clients()
     elif command == 'L':
         list_clients()
     elif command == 'U':
@@ -114,12 +122,10 @@ if __name__ == '__main__':
         updated_client = _get_client_from_user()
 
         update_client(client_id, updated_client)
-        list_clients()
     elif command == 'D':
         client_id = int(_get_client_field('id'))
 
         delete_client(client_id)
-        list_clients()
     elif command == 'S':
         client_name = _get_client_field('name')
         found = search_client(client_name)
@@ -131,3 +137,4 @@ if __name__ == '__main__':
     else:
         print('Invalid command')
 
+    _save_clients_to_storage()
