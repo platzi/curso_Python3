788ce9d54a635de7e79528cbe91d5ef48bea6292
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-02 09:20:31 -0500

    Binary search

---
+++ binary_search.py
@@ -0,0 +1,27 @@
+import random
+
+def binary_search(data, target, low, high):
+    if low > high:
+        return False
+  
+    mid = (low + high) // 2
+
+    if target == data[mid]:
+        return True
+    elif target < data[mid]:
+        return binary_search(data, target, low, mid - 1)
+    else:
+        return binary_search(data, target, mid + 1, high)
+
+
+if __name__ == '__main__':
+    data = [random.randint(0, 100) for i in range(10)]
+
+    data.sort()
+    #data = sorted(data)
+    print(data)
+    target = int(input('what number would you like to find? '))
+
+    found = binary_search(data, target, 0, len(data) - 1)
+    
+    print(found)
